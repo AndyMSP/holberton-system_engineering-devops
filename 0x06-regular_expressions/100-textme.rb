4833bc3ab4198dccd74296d4fee11844d7e936d8
Andy <3775@holbertonschool.com>
2022-06-23 02:40:09 +0000

    Debug

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/from:([^\w]*\w*)\]\s\[to:([^\w]*\w*)\]\s\[flags:(\-*\d:\-*\d:\-*\d:\-*\d:\-*\d)/).join
+puts ARGV[0].scan(/from:([^\w]*\w*)\]\s\[to:([^\w]*\w*)\]\s\[flags:(\-*\d:\-*\d:\-*\d:\-*\d:\-*\d)/).join(",")
